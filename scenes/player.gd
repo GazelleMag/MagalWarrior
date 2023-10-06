529757038cb93207aa8af84e5bc9d155d0473a41
GazelleMag <joao_xxx@hotmail.com>
2023-10-06 19:33:35 +0100

    setup godot to work with vs code and added code formatter

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,20 +4,23 @@ var speed: int = 250
 var player_animation: AnimatedSprite2D
 var last_direction = "down"
 
+
 func _ready():
 	player_animation = $PlayerAnimation
+
 
 func _process(_delta):
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	handle_movement(direction)
 	handle_animation(direction)
-	
-			
+
+
 func handle_movement(direction: Vector2):
 	velocity = direction * speed
 	move_and_slide()
-	
+
+
 func handle_animation(direction: Vector2):
 	if direction != Vector2.ZERO:
 		if abs(direction.x) > abs(direction.y):
