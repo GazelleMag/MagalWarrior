b9ea45cddfe1e6f5e1ab8248616ce103028ce6fb
GazelleMag <joao_xxx@hotmail.com>
2023-10-15 17:39:42 +0100

    added animation for enemy

--- scenes/fighter.gd
+++ scenes/fighter.gd
@@ -1,20 +1,53 @@
 extends CharacterBody2D
 
 const speed: int = 175
+var fighter_animation: AnimatedSprite2D
+var last_direction = "down"
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
 
+func _ready():
+	fighter_animation = $FighterAnimation
+
+
 func _process(_delta: float) -> void:
-	var dir = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = dir * speed
+	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	handle_movement(direction)
+	handle_animation(direction)
+
+
+func handle_movement(direction: Vector2):
+	velocity = direction * speed
 	move_and_slide()
+
+
+func _on_timer_timeout():
+	makepath()
 
 
 func makepath() -> void:
 	nav_agent.target_position = player.global_position
 
 
-func _on_timer_timeout():
-	makepath()
+func handle_animation(direction: Vector2):
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				fighter_animation.play("walk_right")
+				last_direction = "right"
+			else:
+				fighter_animation.play("walk_left")
+				last_direction = "left"
+		else:
+			if direction.y > 0:
+				fighter_animation.play("walk_down")
+				last_direction = "down"
+			else:
+				fighter_animation.play("walk_up")
+				last_direction = "up"
+	else:
+		# Character is idle, choose the idle animation based on the last direction.
+		if last_direction != "":
+			fighter_animation.play("idle_" + last_direction)
